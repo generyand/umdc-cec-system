a55cb298f3dd77d3c1052126c3e434f7086258ff
generyand <generyan.dep@gmail.com>
2024-11-10 11:47:43 +0800

    installed shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+import { motion } from "framer-motion";
+
+import { Button } from "@/components/ui/button";
+
 const App = () => {
-  return <div className="text-3xl font-bold bg-red-700">App</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-3xl font-bold">
+      <p className="mb-4">Hello, World! 🚀</p>
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      >
+        <Button>Click me</Button>
+      </motion.div>
+    </div>
+  );
 };
 
 export default App;

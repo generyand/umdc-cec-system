943a54af48b5e0c61a8597ac8f921337e6798898
generyand <generyan.dep@gmail.com>
2024-11-12 16:15:35 +0800

    Register auth goods

---
+++ server/prisma/migrations/20241112080935_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'FOCAL_PERSON',
+    "refreshToken" TEXT,
+    "resetPasswordToken" TEXT,
+    "resetTokenExpiry" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");

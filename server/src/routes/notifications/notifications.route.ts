4e3ad27cb637af2d19445c7d2839941251db4c08
generyand <generyan.dep@gmail.com>
2024-12-14 17:52:02 +0800

    notifications recipient goodssss

--- server/src/routes/notifications/notifications.route.ts
+++ server/src/routes/notifications/notifications.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getNotifications } from "@/controllers/notifications.controller.js";
+import { authenticateToken } from "@/middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", getNotifications);
+router.get("/", authenticateToken, getNotifications);
 
 export default router;

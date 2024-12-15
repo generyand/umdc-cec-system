af8866ddbcc7eedb26770141f66fbed1cc3e0843
generyand <generyan.dep@gmail.com>
2024-12-15 21:43:01 +0800

    notification route checked

--- server/src/routes/notifications/notifications.route.ts
+++ server/src/routes/notifications/notifications.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getNotifications, markAllAsRead, markAsRead } from "@/controllers/notifications.controller.js";
-import { authenticateToken } from "@/middleware/auth.middleware.js";
+import { authenticateToken } from "../../middleware/auth.middleware.js";
 
 const router = Router();
 
